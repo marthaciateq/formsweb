915167087d0831dfad9f0d44fd02eb475a8a493a
janhell <jangel.hernandez@ciateq.mx>
2016-12-09 13:12:30 -0600

    Se agregaron scripts SQL de la app movil

---
+++ SQL/01 Scripts/0220 - elementsData.sql
@@ -0,0 +1,14 @@
+CREATE TABLE [dbo].[elementsData](
+	[idFormElemento] [char](32) NULL,
+	[idElementoOpcion] [char](32) NULL,
+	[descripcion] [varchar](512) NULL,
+	[fecha] [datetime] NULL,
+	[idUsuario] [char](32) NULL
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+
